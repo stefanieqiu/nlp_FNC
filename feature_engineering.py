0f8241405d8523c92b52fac3f156d9060b669027
stefanieqiu <stefanieqiu@hotmail.com>
2017-09-21 17:43:21 -0400

    Add files via upload
    To generate features

---
+++ feature_engineering.py
@@ -0,0 +1,356 @@
+#!/usr/bin/env Python
+# coding=utf-8
+import os
+import re
+import nltk
+import numpy as np
+from sklearn import feature_extraction
+from tqdm import tqdm
+import nltk
+import yaml
+import sys
+
+_wnl = nltk.WordNetLemmatizer()
+
+
+class Splitter(object):
+    def __init__(self):
+        self.nltk_splitter = nltk.data.load('tokenizers/punkt/english.pickle')
+        self.nltk_tokenizer = nltk.tokenize.TreebankWordTokenizer()
+
+    def split(self, text):
+        sentences = self.nltk_splitter.tokenize(text)
+        tokenized_sentences = [self.nltk_tokenizer.tokenize(sent) for sent in sentences]
+        return tokenized_sentences
+
+
+class POSTagger(object):
+    def __init__(self):
+        pass
+
+    def pos_tag(self, sentences):
+        pos = [nltk.pos_tag(sentence) for sentence in sentences]
+        # adapt format
+        pos = [[(word, word, [postag]) for (word, postag) in sentence] for sentence in pos]
+        return pos
+
+
+class DictionaryTagger(object):
+    def __init__(self, dictionary_paths):
+        files = [open(path, 'r') for path in dictionary_paths]
+        dictionaries = [yaml.load(dict_file) for dict_file in files]
+        map(lambda x: x.close(), files)
+        self.dictionary = {}
+        self.max_key_size = 0
+        for curr_dict in dictionaries:
+            for key in curr_dict:
+                if key in self.dictionary:
+                    self.dictionary[key].extend(curr_dict[key])
+                else:
+                    self.dictionary[key] = curr_dict[key]
+                    self.max_key_size = max(self.max_key_size, len(key))
+        f = open("dicts/positive-words.txt", "r")
+        line = f.readline()  #
+        while line:
+            line = line.strip('\n')
+            self.dictionary[line] = ['positive']
+            self.max_key_size = max(self.max_key_size, len(key))
+            line = f.readline()
+        f.close()
+
+        f_1 = open("dicts/negative-words.txt", "r")
+        line = f_1.readline()
+        while line:
+            line = line.strip('\n')
+            self.dictionary[line] = ['negative']
+            self.max_key_size = max(self.max_key_size, len(key))
+            line = f_1.readline()
+        f_1.close()
+
+    def tag(self, postagged_sentences):
+        return [self.tag_sentence(sentence) for sentence in postagged_sentences]
+
+    def tag_sentence(self, sentence, tag_with_lemmas=False):
+
+        tag_sentence = []
+        N = len(sentence)
+        if self.max_key_size == 0:
+            self.max_key_size = N
+        i = 0
+        while (i < N):
+            j = min(i + self.max_key_size, N)  # avoid overflow
+            tagged = False
+            while (j > i):
+                expression_form = ' '.join([word[0] for word in sentence[i:j]]).lower()
+                expression_lemma = ' '.join([word[1] for word in sentence[i:j]]).lower()
+                if tag_with_lemmas:
+                    literal = expression_lemma
+                else:
+                    literal = expression_form
+                if literal in self.dictionary:
+                    # self.logger.debug("found: %s" % literal)
+                    is_single_token = j - i == 1
+                    original_position = i
+                    i = j
+                    taggings = [tag for tag in self.dictionary[literal]]
+                    tagged_expression = (expression_form, expression_lemma, taggings)
+                    if is_single_token:  # if the tagged literal is a single token, conserve its previous taggings:
+                        original_token_tagging = sentence[original_position][2]
+                        tagged_expression[2].extend(original_token_tagging)
+                    tag_sentence.append(tagged_expression)
+                    tagged = True
+                else:
+                    j = j - 1
+            if not tagged:
+                tag_sentence.append(sentence[i])
+                i += 1
+        return tag_sentence
+
+
+splitter = Splitter()
+postagger = POSTagger()
+dicttagger = DictionaryTagger(['dicts/positive.yml', 'dicts/negative.yml',
+                               'dicts/inc.yml', 'dicts/dec.yml', 'dicts/inv.yml'])
+
+
+def value_of(sentiment):
+    if sentiment == 'positive': return 1
+    if sentiment == 'negative': return -1
+    return 0
+
+
+def sentence_score(sentence_tokens, previous_token, acum_score):
+    if not sentence_tokens:
+        return acum_score
+    else:
+        current_token = sentence_tokens[0]
+        tags = current_token[2]
+        token_score = sum([value_of(tag) for tag in tags])
+        if previous_token is not None:
+            previous_tags = previous_token[2]
+            if 'inc' in previous_tags:
+                token_score *= 2.0
+            elif 'dec' in previous_tags:
+                token_score /= 2.0
+            elif 'inv' in previous_tags:
+                token_score *= -1.0
+        return sentence_score(sentence_tokens[1:], current_token, acum_score + token_score)
+
+
+def sentiment_score(review):
+    return sum([sentence_score(sentence, None, 0.0) for sentence in review])
+
+
+def getscore(text):
+    splitted_sentences = splitter.split(text)
+    pos_tagged_sentences = postagger.pos_tag(splitted_sentences)
+    dict_tagged_sentences = dicttagger.tag(pos_tagged_sentences)
+    score = sentiment_score(dict_tagged_sentences)
+    return score
+
+
+def normalize_word(w):
+    return _wnl.lemmatize(w).lower()
+
+
+def get_tokenized_lemmas(s):
+    return [normalize_word(t) for t in nltk.word_tokenize(s)]
+
+
+def remove_stopwords(l):
+    # Removes stopwords from a list of tokens
+    return [w for w in l if w not in feature_extraction.text.ENGLISH_STOP_WORDS]
+
+
+def gen_or_load_feats(feat_fn, headlines, bodies, feature_file):
+    if not os.path.isfile(feature_file):
+        feats = feat_fn(headlines, bodies)
+        np.save(feature_file, feats)
+
+    return np.load(feature_file)
+
+
+def clean(s):
+    # Cleans a string: Lowercasing, trimming, removing non-alphanumeric
+
+    return " ".join(re.findall(r'\w+', s, flags=re.UNICODE)).lower()  # and add clean back
+
+
+def word_sem_score(headlines, bodies):
+    X = []
+    for i, (headline, body) in tqdm(enumerate(zip(headlines, bodies))):
+        features = []
+        features.append(getscore(headline))
+        features.append(getscore(body))
+        X.append(features)
+    return X
+
+
+def word_overlap_features(headlines, bodies):
+    X = []
+    for i, (headline, body) in tqdm(enumerate(zip(headlines, bodies))):
+        clean_headline = clean(headline)
+        clean_body = clean(body)
+        clean_headline = get_tokenized_lemmas(clean_headline)
+        clean_body = get_tokenized_lemmas(clean_body)
+        features = [
+            len(set(clean_headline).intersection(clean_body)) / float(len(set(clean_headline).union(clean_body)))]
+        X.append(features)
+    return X
+
+
+def refuting_features(headlines, bodies):
+    _refuting_words = [
+        'fake',
+        'fraud',
+        'hoax',
+        'false',
+        'deny', 'denies',
+        # 'refute',
+        'not',
+        'despite',
+        'nope',
+        'doubt', 'doubts',
+        'bogus',
+        'debunk',
+        'pranks',
+        'retract'
+    ]
+    X = []
+    for i, (headline, body) in tqdm(enumerate(zip(headlines, bodies))):
+        clean_headline = clean(headline)
+        clean_headline = get_tokenized_lemmas(clean_headline)
+        features = [1 if word in clean_headline else 0 for word in _refuting_words]
+        X.append(features)
+    return X
+
+
+def polarity_features(headlines, bodies):
+    _refuting_words = [
+        'fake',
+        'fraud',
+        'hoax',
+        'false',
+        'deny', 'denies',
+        'not',
+        'despite',
+        'nope',
+        'doubt', 'doubts',
+        'bogus',
+        'debunk',
+        'pranks',
+        'retract'
+    ]
+
+    def calculate_polarity(text):
+        tokens = get_tokenized_lemmas(text)
+        return sum([t in _refuting_words for t in tokens]) % 2
+
+    X = []
+    for i, (headline, body) in tqdm(enumerate(zip(headlines, bodies))):
+        clean_headline = clean(headline)
+        clean_body = clean(body)
+        features = []
+        features.append(calculate_polarity(clean_headline))
+        features.append(calculate_polarity(clean_body))
+        X.append(features)
+    return np.array(X)
+
+
+def ngrams(input, n):
+    input = input.split(' ')
+    output = []
+    for i in range(len(input) - n + 1):
+        output.append(input[i:i + n])
+    return output
+
+
+def chargrams(input, n):
+    output = []
+    for i in range(len(input) - n + 1):
+        output.append(input[i:i + n])
+    return output
+
+
+def append_chargrams(features, text_headline, text_body, size):
+    grams = [' '.join(x) for x in chargrams(" ".join(remove_stopwords(text_headline.split())), size)]
+    grams_hits = 0
+    grams_early_hits = 0
+    grams_first_hits = 0
+    for gram in grams:
+        if gram in text_body:
+            grams_hits += 1
+        if gram in text_body[:255]:
+            grams_early_hits += 1
+        if gram in text_body[:100]:
+            grams_first_hits += 1
+    features.append(grams_hits)
+    features.append(grams_early_hits)
+    features.append(grams_first_hits)
+    return features
+
+
+def append_ngrams(features, text_headline, text_body, size):
+    grams = [' '.join(x) for x in ngrams(text_headline, size)]
+    grams_hits = 0
+    grams_early_hits = 0
+    for gram in grams:
+        if gram in text_body:
+            grams_hits += 1
+        if gram in text_body[:255]:
+            grams_early_hits += 1
+    features.append(grams_hits)
+    features.append(grams_early_hits)
+    return features
+
+
+def hand_features(headlines, bodies):
+    def binary_co_occurence(headline, body):
+        # Count how many times a token in the title
+        # appears in the body text.
+        bin_count = 0
+        bin_count_early = 0
+        for headline_token in clean(headline).split(" "):
+            if headline_token in clean(body):
+                bin_count += 1
+            if headline_token in clean(body)[:255]:
+                bin_count_early += 1
+        return [bin_count, bin_count_early]
+
+    def binary_co_occurence_stops(headline, body):
+        # Count how many times a token in the title
+        # appears in the body text. Stopwords in the title
+        # are ignored.
+        bin_count = 0
+        bin_count_early = 0
+        for headline_token in remove_stopwords(clean(headline).split(" ")):
+            if headline_token in clean(body):
+                bin_count += 1
+                bin_count_early += 1
+        return [bin_count, bin_count_early]
+
+    def count_grams(headline, body):
+        # Count how many times an n-gram of the title
+        # appears in the entire body, and intro paragraph
+
+        clean_body = clean(body)
+        clean_headline = clean(headline)
+        features = []
+        features = append_chargrams(features, clean_headline, clean_body, 2)
+        features = append_chargrams(features, clean_headline, clean_body, 8)
+        features = append_chargrams(features, clean_headline, clean_body, 4)
+        features = append_chargrams(features, clean_headline, clean_body, 16)
+        features = append_ngrams(features, clean_headline, clean_body, 2)
+        features = append_ngrams(features, clean_headline, clean_body, 3)
+        features = append_ngrams(features, clean_headline, clean_body, 4)
+        features = append_ngrams(features, clean_headline, clean_body, 5)
+        features = append_ngrams(features, clean_headline, clean_body, 6)
+        return features
+
+    X = []
+    for i, (headline, body) in tqdm(enumerate(zip(headlines, bodies))):
+        X.append(binary_co_occurence(headline, body)
+                 + binary_co_occurence_stops(headline, body)
+                 + count_grams(headline, body))
+
+    return X
